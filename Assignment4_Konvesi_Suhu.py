3fdb3044829a2ae7c4b74c085f2aab7f7769cebd
fahmy009 <muhammadfahmy009@gmail.com>
2023-11-18 23:53:33 +0700

    Update Terbaru

--- Assignment4_Konvesi_Suhu.py
+++ Assignment4_Konvesi_Suhu.py
@@ -33,11 +33,11 @@ if nilai_input <= 100:  # Kondisi jika nilai input antara 0 - 100 dinyatakan Cel
     print(f"Suhu akan di konversi ke Kelvin dan Farenheit")
     print(f"---------------------------------------------------------------")
     print(
-        f"Suhu hasil konversi ke Kelvin adalah {float(celcius_ke_kelvin(nilai_input))} K"
+        f"Suhu hasil konversi ke Kelvin adalah {float(celcius_ke_kelvin(nilai_input)):.2f} K"
     )
     print(f"---------------------------------------------------------------")
     print(
-        f"Suhu hasil konversi ke Farenheit adalah {float(celcius_ke_farenheit(nilai_input))} F"
+        f"Suhu hasil konversi ke Farenheit adalah {float(celcius_ke_farenheit(nilai_input)):.2f} F"
     )
 elif (
     nilai_input >= 273 and nilai_input <= 373
@@ -47,11 +47,11 @@ elif (
     print(f"Suhu akan di konversi ke Celcius dan Farenheit")
     print(f"---------------------------------------------------------------")
     print(
-        f"Suhu hasil konversi ke Celcius adalah {float(kelvin_ke_celcius(nilai_input))} C"
+        f"Suhu hasil konversi ke Celcius adalah {float(kelvin_ke_celcius(nilai_input)):.2f} C"
     )
     print(f"---------------------------------------------------------------")
     print(
-        f"Suhu hasil konversi ke Farenheit adalah {float(kelvin_ke_farenheit(nilai_input))} F"
+        f"Suhu hasil konversi ke Farenheit adalah {float(kelvin_ke_farenheit(nilai_input)):.2f} F"
     )
 else:  # Kondisi jika nilai input antara 100 - 273 dinyatakan Farenheit
     print(f"Jenis Suhu Yang Diinput adalah Farenheit dengan {nilai_input} Derajat")
@@ -59,9 +59,9 @@ else:  # Kondisi jika nilai input antara 100 - 273 dinyatakan Farenheit
     print(f"Suhu akan di konversi ke Celcius dan Kelvin")
     print(f"---------------------------------------------------------------")
     print(
-        f"Suhu hasil konversi ke Celcius adalah {float(farenheit_ke_celcius(nilai_input))} C"
+        f"Suhu hasil konversi ke Celcius adalah {float(farenheit_ke_celcius(nilai_input)):.2f} C"
     )
     print(f"---------------------------------------------------------------")
     print(
-        f"Suhu hasil konversi ke Kelvin adalah {float((farenheit_ke_kelvin(nilai_input)))} F"
+        f"Suhu hasil konversi ke Kelvin adalah {float((farenheit_ke_kelvin(nilai_input))):.2f} F"
     )
